03079ad87c34ac905739c23091f9f943a66d43b6
Ben Thornburg <ben@okeedoke.com>
2018-11-10 13:49:36 -0500

    super simple completion toggle

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 class GameItem extends Component {
+  componentWillMount() {
+    this.setState({ completed: this.props.game.dateCompleted > 0 })
+  }
+
+  itemClicked = e => {
+    this.setState({ completed: !this.state.completed })
+  }
+
   render() {
     const { game } = this.props;
+    const { completed } = this.state;
+
     return (
-        <div className={`GameItem ${game.dateCompleted > 0 ? `completed` : ``}`}>
+        <div className={`GameItem ${completed ? `completed` : ``}`} onClick={this.itemClicked}>
           {game.title}
         </div>
     );
