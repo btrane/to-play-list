39c903b5701155923e3d2b0d6fbcc204dfb8e66e
Ben Thornburg <ben@okeedoke.com>
2018-11-12 22:00:23 -0500

    make initialStore mst compatible

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,6 @@
 const initialState = {
-  games: [
-    {
+  games: {
+    "1": {
       id: 1,
       title: 'The Legend of Zelda: Breath of the Wild',
       image: 'https://images.igdb.com/igdb/image/upload/t_cover_big/mievpzb9rbzzenmznvnr.jpg',
@@ -8,7 +8,7 @@ const initialState = {
       dateAdded: 1541868941707,
       dateCompleted: 0,
     },
-    {
+    "2" : {
       id: 2,
       title: 'Red Dead Redemption 2',
       image: 'https://howlongtobeat.com/gameimages/27100_Red_Dead_Redemption_2.png',
@@ -16,7 +16,7 @@ const initialState = {
       dateAdded: 1541868941100,
       dateCompleted: 0,
     },
-    {
+    "3" : {
       id: 3,
       title: 'Undertale',
       image: 'https://howlongtobeat.com/gameimages/Undertale_header.jpg',
@@ -24,7 +24,7 @@ const initialState = {
       dateAdded: 1541868941301,
       dateCompleted: 1541868941769,
     },
-    {
+    "4" : {
       id: 4,
       title: 'Just Dance 2018',
       image: 'https://howlongtobeat.com/gameimages/50164_Just_Dance_2018.jpg',
@@ -32,7 +32,7 @@ const initialState = {
       dateAdded: 1541873631507,
       dateCompleted: 0,
     },
-    {
+    "5" : {
       id: 5,
       title: 'Kirby\'s Adventure',
       image: 'https://howlongtobeat.com/gameimages/250px-Kirbys_Adventure_Coverart.png',
@@ -40,11 +40,13 @@ const initialState = {
       dateAdded: 1541873750004,
       dateCompleted: 0,
     },
-  ],
+  },
   profile: {
-    userName: 'Ben',
-    completionType: 'MAIN', // MAIN, EXTRA or COMPLETE
-    orderBy: 'DATE', // DATE, TIME or TITLE
+    "0" : {
+      userName: 'Ben',
+      completionType: 'MAIN', // MAIN, EXTRA or COMPLETE
+      orderBy: 'DATE', // DATE, TIME or TITLE
+    }
   }
 }
 
